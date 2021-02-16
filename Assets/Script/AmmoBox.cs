290fd19926f4d5253ab3c538ce0f6c9abdc38f3f
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-02-16 09:41:12 -0400

    Added a collectible with movement and texture

---
+++ Assets/Script/AmmoBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    private float rotationSpeed = 100f;
+    private float moveSpeed = 0.5f;
+    // used in the movement scripts
+    private bool startedMoveLoop;
+    private bool moveDown;
+    private bool moveUp;
+
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos.y = gameObject.transform.position.y; // Allows prefab to be placed at any height
+    }
+
+    private void Update()
+    {
+        // Used to rotate the box
+        gameObject.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        // For Moving the box up then down
+        if (!startedMoveLoop) gameObject.transform.Translate(Vector3.up * Time.deltaTime);
+        if (gameObject.transform.position.y >= startPos.y + 0.5)
+        {
+            startedMoveLoop = true;
+            moveDown = true;
+            moveUp = false;
+        }
+        else if (gameObject.transform.position.y <= startPos.y)
+        {
+            moveUp = true;
+            moveDown = false;
+        }
+        if (moveDown) gameObject.transform.Translate(Vector3.down * Time.deltaTime * moveSpeed) ;
+        if (moveUp) gameObject.transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+    }
+    
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject spawnLocationObj;
+    public CharacterController characterController;
+
+    private float ammo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("KillBox"))
+        {
+
+            Vector3 spawnDistance = spawnLocationObj.transform.position - gameObject.transform.position;
+            characterController.Move(spawnDistance);
+            Debug.Log("Triggered KillBox");
+        }
+
+        if (other.CompareTag("Checkpoint"))
+        {
+            spawnLocationObj.transform.position = other.transform.position;
+            Debug.Log("Triggered Checkpoint");
+        }
+
+        if (other.CompareTag("AmmoBox"))
+        {
+            ammo += 30;
+            Debug.Log("Collected Ammo");
+        }
+    }
+
+
+    private void Start()
+    {
+        ammo = 60;
+    }
+}
