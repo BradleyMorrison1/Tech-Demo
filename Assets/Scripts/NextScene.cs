9419cc210987efa8e842007e97a2ae3fdc93c47f
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-03-05 14:48:41 -0400

    Added trigger to swap levels and splash screen

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelSwap : MonoBehaviour
+public class NextScene : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
         }
     }
 }
